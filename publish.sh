f35b26eaa25c0a3749950314117d84a2c70fa1f7
Milan Jurkulak <mimoccc@gmail.com>
2023-06-02 03:33:42 +0200

    new release

--- publish.sh
+++ publish.sh
@@ -2,8 +2,8 @@
 export GRADLE_PATH=./version.properties
 export GRADLE_FIELD="versionName"
 # logic
-export VERSION_TMP=$(grep $GRADLE_FIELD $GRADLE_PATH | awk '{print $2}')
-export VERSION=$(echo $VERSION_TMP | sed -e 's/^"//'  -e 's/"$//')
+export VERSION_TMP=$(grep $GRADLE_FIELD $GRADLE_PATH | cat $2)
+export VERSION=$(echo $VERSION_TMP | sed -e 's/^versionName=//'  -e 's/"$//')
 # result
 echo release tag: $VERSION
 # release
