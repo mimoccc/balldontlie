7eacae56f90f970b6c1412de0702e658c22cd1da
Milan Jurkulak <mimoccc@gmail.com>
2023-06-05 17:10:09 +0200

    dao improvements

--- app/src/main/java/org/mjdev/balldontlie/application/MainApp.kt
+++ app/src/main/java/org/mjdev/balldontlie/application/MainApp.kt
@@ -3,11 +3,8 @@ package org.mjdev.balldontlie.application
 import android.app.Application
 import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
-import org.mjdev.balldontlie.R
-import org.mjdev.balldontlie.base.helpers.Ext.stringResource
 import org.mjdev.balldontlie.sync.SyncService
 import org.mjdev.balldontlie.sync.SyncService.Companion.createSyncAccount
-import net.sqlcipher.database.SQLiteDatabase
 
 @HiltAndroidApp
 class MainApp : Application() {
@@ -23,23 +20,7 @@ class MainApp : Application() {
     }
 
     private fun initializeDBSQLCipher() {
-        val name = stringResource(R.string.app_name)
-        val dbName = "$name.db"
         System.loadLibrary("sqlcipher")
-        val databaseFile = getDatabasePath(dbName)
-        val database = SQLiteDatabase.openOrCreateDatabase(
-            databaseFile,
-            name,
-            null,
-            null
-        )
-        if (database != null) {
-            database.execSQL("create table t1(a,b);")
-            database.execSQL(
-                "insert into t1(a,b) values(?, ?);",
-                arrayOf("", "")
-            )
-        }
     }
 
 }
--- app/src/main/java/org/mjdev/balldontlie/database/DAO.kt
+++ app/src/main/java/org/mjdev/balldontlie/database/DAO.kt
@@ -1,18 +1,25 @@
+@file:Suppress("unused")
+
 package org.mjdev.balldontlie.database
 
 import android.content.Context
+import android.os.Environment
 import com.j256.ormlite.cipher.android.apptools.OrmLiteSqliteOpenHelper
 import com.j256.ormlite.dao.Dao
 import com.j256.ormlite.support.ConnectionSource
 import com.j256.ormlite.table.TableUtils
+import dagger.hilt.android.qualifiers.ApplicationContext
+import net.sqlcipher.database.SQLiteDatabase
 import org.mjdev.balldontlie.R
 import org.mjdev.balldontlie.base.helpers.Ext.stringResource
 import org.mjdev.balldontlie.model.Player
 import org.mjdev.balldontlie.model.Team
 import timber.log.Timber
+import java.io.File
 
 class DAO(
-    val context: Context
+    @ApplicationContext
+    val context: Context,
 ) : OrmLiteSqliteOpenHelper(
     context,
     DATABASE_NAME,
@@ -21,12 +28,26 @@ class DAO(
     R.raw.ormlite_config
 ) {
 
+    companion object {
+
+        const val DATABASE_NAME = "balldontlie"
+        const val DATABASE_VERSION = 1
+        // note external db needs permission
+        val EXTERNAL_DB = Environment.getExternalStorageDirectory().absolutePath +
+                File.separator +
+                DATABASE_NAME
+        const val INTERNAL_DB = DATABASE_NAME
+
+    }
+
+    private val dbLocation: String = EXTERNAL_DB
+
     val playerDao: Dao<Player, Int> get() = getDao(Player::class.java)
 
     val teamDao: Dao<Team, Int> get() = getDao(Team::class.java)
 
     override fun onCreate(
-        database: net.sqlcipher.database.SQLiteDatabase?,
+        database: SQLiteDatabase?,
         connectionSource: ConnectionSource?
     ) {
         try {
@@ -38,7 +59,7 @@ class DAO(
     }
 
     override fun onUpgrade(
-        database: net.sqlcipher.database.SQLiteDatabase?,
+        database: SQLiteDatabase?,
         connectionSource: ConnectionSource?,
         oldVersion: Int,
         newVersion: Int
@@ -52,11 +73,27 @@ class DAO(
         }
     }
 
-    override fun getPassword(): String = context.stringResource(R.string.database_password)
-
-    companion object {
-        private const val DATABASE_NAME = "balldontlie"
-        private const val DATABASE_VERSION = 1
+    @Synchronized
+    fun getWritableDatabase(): SQLiteDatabase? {
+        return SQLiteDatabase.openDatabase(
+            dbLocation,
+            password,
+            null,
+            SQLiteDatabase.OPEN_READWRITE
+        )
     }
+
+    @Synchronized
+    fun getReadableDatabase(): SQLiteDatabase? {
+        return SQLiteDatabase.openDatabase(
+            dbLocation,
+            password,
+            null,
+            SQLiteDatabase.OPEN_READONLY
+        )
+    }
+
+
+    override fun getPassword(): String = context.stringResource(R.string.database_password)
 
 }
