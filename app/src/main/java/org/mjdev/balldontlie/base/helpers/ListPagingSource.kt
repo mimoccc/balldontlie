7c6f65bc8d85badd3795e39361cb11ddb8ebe651
Milan Jurkulak <mimoccc@gmail.com>
2023-06-05 03:53:58 +0200

    merge fixes :) external tool, github error

--- app/src/main/java/org/mjdev/balldontlie/base/helpers/ListPagingSource.kt
+++ app/src/main/java/org/mjdev/balldontlie/base/helpers/ListPagingSource.kt
@@ -2,13 +2,21 @@ package org.mjdev.balldontlie.base.helpers
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import kotlinx.coroutines.delay
 
 typealias SOURCE<T> = suspend (page: Int, cnt: Int) -> List<T>
 
 class ListPagingSource<T : Any>(
     private val perPage: Int = 50,
     private val source: SOURCE<T> = { _, _ -> emptyList() },
+    private val maxRetryCount: Int = DEFAULT_MAX_RETRY_COUNT,
+    private val retryDelay: Long = DEFAULT_RETRY_DELAY,
 ) : PagingSource<Int, T>() {
+
+    companion object {
+        const val DEFAULT_MAX_RETRY_COUNT = 10
+        const val DEFAULT_RETRY_DELAY = 250L // 250 ms
+    }
 
     override fun getRefreshKey(state: PagingState<Int, T>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
@@ -20,7 +28,13 @@ class ListPagingSource<T : Any>(
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, T> {
         return try {
             val page = params.key ?: 1
-            val response = source.invoke(page, perPage)
+            var maxTryCount = maxRetryCount
+            var response = source.invoke(page, params.loadSize)
+            while ((response.isEmpty() || response.size < params.loadSize) && (maxTryCount > 0)) {
+                delay(retryDelay)
+                maxTryCount -= 1
+                response = source.invoke(page, params.loadSize)
+            }
             return LoadResult.Page(
                 data = response,
                 prevKey = if (page == 1) null else page.minus(1),
